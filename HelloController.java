d3814bdaefc8159dae427f362638f77777cdf77d
robinhudec <133644866+robinhudec@users.noreply.github.com>
2023-10-08 15:49:38 +0200

    Pridani About tlacitka, oprava praveho sloupce a select tlacitka

--- HelloController.java
+++ HelloController.java
@@ -3,12 +3,12 @@ package com.example.mrdkapropepeho;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.FileChooser;
 import java.io.File;
 import javafx.stage.Stage;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.StackPane;
 
 public class HelloController {
     public Stage stage;
@@ -24,5 +24,15 @@ public class HelloController {
     @FXML
     private void handleQuit(ActionEvent event){
         Platform.exit();
+    }
+
+    @FXML
+    private void handleAbout(ActionEvent event){
+        Stage newStage = new Stage();
+        Label aboutUs = new Label("nase info nevim presne");
+        StackPane newLayout = new StackPane(aboutUs);
+        Scene newScene = new Scene(newLayout, 300, 200);
+        newStage.setScene(newScene);
+        newStage.show();
     }
 }
