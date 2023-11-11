d3232bba481baf15d7e7f57bdcd954b45b8c9af7
robinhudec <133644866+robinhudec@users.noreply.github.com>
2023-11-11 12:54:40 +0100

    test spinneru

--- mrdka pro pepeho/src/main/java/com/example/mrdkapropepeho/HelloController.java
+++ mrdka pro pepeho/src/main/java/com/example/mrdkapropepeho/HelloController.java
@@ -4,16 +4,22 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.stage.FileChooser;
 import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-public class HelloController {
+public class HelloController implements Initializable {
     public Stage stage;
     @FXML
     private Label welcomeText;
@@ -49,4 +55,25 @@ public class HelloController {
         aboutStage.show();
     }
 
+    @FXML
+    private void handleEditMatrix(ActionEvent event){
+        Stage editMatrix = new Stage();
+        editMatrix.setMinWidth(300);
+        editMatrix.setMaxWidth(300);
+        editMatrix.setMinHeight(200);
+        editMatrix.setMaxHeight(200);
+
+        editMatrix.show();
+    }
+    @FXML
+    public Spinner<Integer> numberSpinner;
+
+    int currentValue;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(-100, 100);
+        valueFactory.setValue(0);
+        numberSpinner.setValueFactory(valueFactory);
+        }
 }
